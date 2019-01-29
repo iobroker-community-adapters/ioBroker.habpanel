058c1c6291814b4e74687201abdb25915b205165
Matthias Gasser <matthias.gasser@gmail.com>
2019-01-29 20:49:58 +0100

    time series test

--- src/app/widgets/chart/chart.widget.js
+++ src/app/widgets/chart/chart.widget.js
@@ -117,7 +117,7 @@
 
                 vm.rawdata = [];
                 for (var i = 0; i < vm.widget.series.length; i++) {
-                    vm.rawdata[i] = OHService.getTimeSeries(vm.widget.service, vm.widget.series[i].item, startDate.toISOString());
+                    vm.rawdata[i] = OHService.getTimeSeries(vm.widget.service, vm.widget.series[i].item, startDate.getTime());
                     //vm.rawdata[i] = $http.get('/rest/persistence/items/' + vm.widget.series[i].item + "?starttime=" + startDate.toISOString() + (vm.widget.service ? '&serviceId=' + vm.widget.service : ''));
                 }
 
