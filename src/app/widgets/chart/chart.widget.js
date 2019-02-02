dd0a2a4d7b8f5a6219db7c1c5d286f3eda69a5df
Matthias Gasser <matthias.gasser@gmail.com>
2019-02-03 00:24:10 +0100

    add missing )

--- src/app/widgets/chart/chart.widget.js
+++ src/app/widgets/chart/chart.widget.js
@@ -116,7 +116,7 @@
             var getData = function () {
                 vm.rawdata = [];
                 for (var i = 0; i < vm.widget.series.length; i++) {
-                    vm.rawdata[i] = OHService.getTimeSeries(vm.widget.service, vm.widget.series[i].item, startDate.getTime, new Date().getTime();
+                    vm.rawdata[i] = OHService.getTimeSeries(vm.widget.service, vm.widget.series[i].item, startDate.getTime, new Date().getTime());
                     //vm.rawdata[i] = $http.get('/rest/persistence/items/' + vm.widget.series[i].item + "?starttime=" + startDate.toISOString() + (vm.widget.service ? '&serviceId=' + vm.widget.service : ''));
                 }
 
